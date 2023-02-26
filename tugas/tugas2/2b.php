d5e7ea87c3a3395755f51c5a6212cae11e2a6647
Davinaputku <davinaputri2408@gmail.com>
2023-02-26 21:57:30 +0700

    tugas2

---
+++ tugas/tugas2/2b.php
@@ -0,0 +1,11 @@
+<?php 
+
+$line = 10;
+
+for ($number = 1; $number <= $line; $number++) {
+    for ($column = 1; $column <= $number; $column++) {
+        echo "$column ";
+    }
+    echo "<br>";
+}
+?>
